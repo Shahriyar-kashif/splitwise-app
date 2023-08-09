78c66359424031d0c13f90c97f503b0f3eec2d15
Shahriyar Kashif <shahriyar.intern@devsinc.com>
2023-08-09 14:17:25 +0500

    SignupPage: Added signup page with functionality and restructured the files in the app

---
+++ src/components/SignupForm/SignupForm.jsx
@@ -0,0 +1,122 @@
+import {
+  Container,
+  CssBaseline,
+  Typography,
+  Box,
+  Grid,
+  TextField,
+  Button,
+  Link,
+} from "@mui/material";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase/firebase";
+import { Link as RouterLink } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function SignupForm() {
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const email = formData.get("email");
+    const password = formData.get("password");
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredentials) => {
+        const user = userCredentials.user;
+        console.log(user);
+        if (error) setError(null);
+        navigate("/login");
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  };
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          mt: 15,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography component="h1" variant="h4">
+          Sign up
+        </Typography>
+        <Box component="form" sx={{ mt: 3 }} onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                name="firstName"
+                required
+                fullWidth
+                id="firstName"
+                label="First Name"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                required
+                fullWidth
+                id="lastName"
+                label="Last Name"
+                name="lastName"
+                autoComplete="family-name"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="password"
+                label="Password"
+                type="password"
+                name="password"
+                autoComplete="new-password"
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign up
+          </Button>
+          {error && (
+            <Typography
+              component="p"
+              sx={{ color: "red", textAlign: "center" }}
+            >
+              {error}
+            </Typography>
+          )}
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Link variant="body2" component={RouterLink} to="/login">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,36 +2,37 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Root from "./routes/Root";
-import Signin from './components/Signin';
+import Root from "./routes/Root/Root";
 import { ThemeProvider } from "@emotion/react";
 import { theme } from "./themes/theme";
-import Signup from "./components/Signup";
-import Home from "./routes/Home";
+import Home from "./routes/Home/Home";
+import Signup from "./routes/Signup/Signup";
+import Signin from "./routes/Signin/Signin";
+
 const router = createBrowserRouter([
   {
-    path:"/",
+    path: "/",
     element: <Root />,
-    children:[
+    children: [
       {
-        path:'/',
-        element:<Home />,
+        path: "/",
+        element: <Home />,
       },
       {
-        path:'/login',
-        element:<Signin />,
+        path: "/login",
+        element: <Signin />,
       },
       {
-        path:'/signup',
-        element:<Signup />
-      }
-    ]
-  }
-])
+        path: "/signup",
+        element: <Signup />,
+      },
+    ],
+  },
+]);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-    <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </ThemeProvider>
   </React.StrictMode>
 );
