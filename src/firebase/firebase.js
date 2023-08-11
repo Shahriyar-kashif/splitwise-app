2ba59ee87e6b2adc46edc49a45e37a5c50d96d03
Shahriyar Kashif <shahriyar.intern@devsinc.com>
2023-08-11 18:39:49 +0500

    FirebaseConfigUpdate: Added firebase config keys in env file and imported env variables from that file to be used for firebase config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,15 +1,18 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
 const firebaseConfig = {
-    apiKey: "AIzaSyALFHZPtzHoAWcRvPNxcOOV8TpePSGEmN0",
-    authDomain: "splitwise-app-facc4.firebaseapp.com",
-    projectId: "splitwise-app-facc4",
-    storageBucket: "splitwise-app-facc4.appspot.com",
-    messagingSenderId: "234930113671",
-    appId: "1:234930113671:web:2a382ac4291a16d9d3bc22",
-    measurementId: "G-6NP8WKS2WH",
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
+
 const firebaseApp = initializeApp(firebaseConfig);
 export const db = getFirestore(firebaseApp);
 export const auth = getAuth(firebaseApp);
+export const storage = getStorage(firebaseApp);
